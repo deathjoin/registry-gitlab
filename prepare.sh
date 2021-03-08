c30be18b78888f0e16a396aab4e72fbc762f5b4d
Vyacheslav Gerasimov <deathjoin@gmail.com>
2021-03-09 00:34:26 +0500

    init commit

---
+++ prepare.sh
@@ -0,0 +1,4 @@
+yum install -y openssl httpd-tools
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout auth/domain.key -x509 -days 365 -out auth/domain.crt
+htpasswd -Bn ruser > auth/nginx.htpasswd
+mkdir -p {registry-data,postgresql,gitlab-data,gitlab-logs,gitlab-config}
