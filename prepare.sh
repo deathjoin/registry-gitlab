465a492c7f48d104e9c34ba2086568a1259f7d5b
Vyacheslav Gerasimov <deathjoin@gmail.com>
2021-03-10 15:24:32 +0500

    postgresql → postgresql-data

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 yum install -y openssl httpd-tools
 openssl req -newkey rsa:4096 -nodes -sha256 -keyout auth/domain.key -x509 -days 365 -out auth/domain.crt
 htpasswd -Bn ruser > auth/nginx.htpasswd
-mkdir -p {registry-data,postgresql,gitlab-data,gitlab-logs,gitlab-config,gitlab-runner-config}
+mkdir -p {registry-data,postgresql-data,gitlab-data,gitlab-logs,gitlab-config,gitlab-runner-config}
 touch gitlab-runner-config/docker-runner.toml
